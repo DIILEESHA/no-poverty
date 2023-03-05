0c37ba6d85e9da536d721c93d5aaf25cb6ca07a5
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-03-05 22:28:31 +0530

    added file structure and home view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./app.css";
+import Campaign from "./components/campaign/Campaign";
+import Subfooter from "./components/footer/Subfooter/Subfooter";
+import Header from "./components/Header/Header";
+import Navheader from "./components/Nav/Navheader/Navheader";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navheader />
+      <Header />
+      <Campaign />
+      <Subfooter />
     </div>
   );
 }
