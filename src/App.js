2c95f14beb4b6f89e4a50a4100415a92f8d90c00
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-03-06 11:02:40 +0530

    create footer comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./app.css";
 import Campaign from "./components/campaign/Campaign";
+import Maingooter from "./components/footer/Mainfooter/Maingooter";
 import Subfooter from "./components/footer/Subfooter/Subfooter";
 import Header from "./components/Header/Header";
 import Navheader from "./components/Nav/Navheader/Navheader";
@@ -9,6 +10,7 @@ function App() {
       <Navheader />
       <Header />
       <Campaign />
+      <Maingooter />
       <Subfooter />
     </div>
   );
