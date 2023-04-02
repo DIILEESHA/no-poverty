249408a8ec4e3bd1528c07c71f442c58775e5c3e
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-04-03 02:00:39 +0530

    Login page created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Maingooter from "./components/footer/Mainfooter/Maingooter";
 import Subfooter from "./components/footer/Subfooter/Subfooter";
 import Combinenav from "./components/Nav/Combinednav/Combinenav";
 import Home from "./pages/Home/Home";
+import Login from "./components/logger/Login/Login";
 function App() {
   return (
     <div className="App">
-      <Combinenav />
-      <Home />
-      <Maingooter />
-      <Subfooter />
+      {/* <Combinenav /> */}
+      {/* <Home /> */}
+      {/* <Maingooter /> */}
+      {/* <Subfooter /> */}
+      <Login />
     </div>
   );
 }
--- src/components/logger/Login/Login.js
+++ src/components/logger/Login/Login.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import './login.css'
+import React from "react";
+import "./login.css";
+import logo from "../../../assets/log.png";
 
 const Login = () => {
   return (
-    <div className='login__container'>Login</div>
-  )
-}
+    <div className="login__container">
+      <div className="login__left">
+        <div className="login__left__container">
+          <h1 className="login__title">login</h1>
+          <div className="log__conditions">
+            <label className="login__label">Email</label>
+            <input
+              type="email"
+              placeholder="Example@gmail.com"
+              className="login__input__combiner"
+            />
+          </div>
+          <div className="log__conditions">
+            <label className="login__label">Password</label>
+            <input
+              type="password"
+              placeholder="*****"
+              className="login__input__combiner"
+            />
+          </div>
+          <button className="log__btn">login</button>
+          <span className="log__span">
+            Have not an Account?
+            <span
+              style={{
+                color: "#0C6E68",
+                padding: "0px 2px",
+                cursor: "pointer",
+              }}
+            >
+              Sign Up
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className="login__right">
+        <div className="login__right__container">
+          <div className="log__imager">
+            <img src={logo} alt="log__image" className="log__img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
