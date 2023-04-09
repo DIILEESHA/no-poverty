61035281d074dc512e7d332cf9eedf26e99edec6
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-04-09 17:34:25 +0530

    added

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import "./app.css";
+import Maingooter from "./components/footer/Mainfooter/Maingooter";
+import Subfooter from "./components/footer/Subfooter/Subfooter";
+import Combinenav from "./components/Nav/Combinednav/Combinenav";
+import Home from "./pages/Home/Home";
+import Login from "./components/logger/Login/Login";
+import Signup from "./components/logger/Signup/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import ViewSession from "./components/sessions/ViewSession/ViewSession";
+import SingleSession from "./components/sessions/SingleSession/SingleSession";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Routes>
+        
+      </Routes> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Layout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sessions" element={<ViewSession />} />
+          <Route path="/session" element={<SingleSession />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav/Mainnav/Mainnav.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./mainnav.css";
+import { Link } from "react-router-dom";
+
+const Mainnav = () => {
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 1) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+
+  const goContact = () => {
+    window.location = "/sessions";
+  };
+  return (
+    <div
+      className={
+        colorChange
+          ? "main__nav__container colorChange"
+          : "main__nav__container"
+      }
+    >
+      <div className="navlogo__title__section">
+        <h2 className="navlogo__title">endpoverty</h2>
+      </div>
+      <div className="nav__ul__section">
+        <Link to="/">
+          <li className="nav__li__section">home</li>
+        </Link>
+        <li className="nav__li__section">about us </li>
+        <li className="nav__li__section">contact us</li>
+        <Link to="/sessions">
+          <li className="nav__li__section">sessions</li>
+        </Link>
+        <li className="nav__li__section">jobs</li>
+      </div>
+      <div className="nav__credention__btn__section">
+        <Link to="/login">
+          <button className="nav__btn">login</button>
+        </Link>
+        <Link to="/Signup">
+          <button className="nav__btn">register</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Mainnav;
