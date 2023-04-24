c252a5c163bc0c4bee9eb0fd87e2e343a3672fba
Shehan_Weerasinghe <IT20641924@my.sliit.lk>
2023-04-25 05:20:28 +0530

    modify

--- client/src/components/sehan/Edit.js
+++ client/src/components/sehan/Edit.js
@@ -1,60 +1,170 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
-import{Link} from "react-router-dom";
-import swal from 'sweetalert2';
+import{Link,useParams} from "react-router-dom";
+import Swal from 'sweetalert2';
+import axios from "axios";
 
 const  Edit= () => {
-  function selfie()
- {
-  swal.fire({
-    title: "Updated!",
-    text: "You clicked the button!",
-    icon: "success",
-    button: "OK",
-  })
- }
+  const [productname, setProductname] = useState("");
+  const [unitprice, setUnitprice] = useState("");
+  const [sellername, setSellername] = useState("");
+  const [contactno, setContactno] = useState("");
+  const [description, setDescription] = useState("");
+  const { id } = useParams();
+
+
+  useEffect(() => {
+    function getProduct() {
+      axios.get(`http://localhost:5000/product/get/${id}`).then((res) => {
+
+        if (res.data.status) {
+          setProductname(res.data.product.productname);
+          setUnitprice(res.data.product.unitprice);
+          setSellername(res.data.product.sellername);
+          setContactno(res.data.product.contactno);
+          setDescription(res.data.product.description);
+          
+        }
+      }).catch((err)=>{
+
+        alert(err);
+      });
+    }
+
+    getProduct();
+  },[]);
+
+  function update(e) {
+
+    e.preventDefault();
+
+    const data = {
+      productname,
+      unitprice,
+      sellername,
+      contactno,
+      description
+    };
+
+    axios.put(`http://localhost:5000/product/update/${id}`, data).then(() => {
+
+      Swal.fire({
+
+        title: "Success!",
+        text: "Updated Successfully",
+        icon: "success",
+        showConfirmButton: false,
+      });
+
+
+
+    }).catch((err) => {
+
+      Swal.fire({
+        title: "Error!",
+        text: "Couldn't Update your Details",
+        type: "error",
+      });
+    });
+
+    setTimeout(() => {
+      window.location.replace("/view");
+    }, 2500)
+  }
 
   return (
     
-  <div>
+    <div id="body3">
    
-     <form id="f1">
-     <h1 id="fz">Product Details</h1>
+   <form id="f1" onSubmit={update}>
+     <h1 id="fz">Update Details</h1>
       <img src="https://i.pinimg.com/736x/b9/02/b9/b902b9aad58334dd19ad738bfb0083a2--broom-corn-things-i-want.jpg"  id="imgd"/>
 
-      <table id="tb1">
-         <tr id="tr1">
-           <td id="td1">Product Name</td>
-           <td id="td1">  Broom </td>
-         </tr>
+      
+            
 
-         <tr id="tr1">
-           <td id="td1">Unit Price</td>
-           <td id="td1">  Rs:300.00 </td>
-         </tr>
-
-         <tr id="tr1">
-           <td id="td1">Seller Name</td>
-           <td id="td1"> Kasun </td>
-         </tr>
-
-         <tr id="tr1">
-           <td id="td1">Contact Number</td>
-           <td id="td1"> 0719081929 </td>
-         </tr>
-
-         <tr id="tr1">
-           <td id="td1">Discription</td>
-           <td id="td1"> Good product </td>
-         </tr>
-      </table>
+            <div class="form-group">
+              <input
+                type="text"
+                value={productname}
+                class="form-control"
+                id="inputproductname"
+                onChange={(e) => {
+                  setProductname(e.target.value);
+                }}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="Number"
+                value={unitprice}
+                class="form-control"
+                id="inputunitprice"
+                placeholder="Unit Price"
+                onChange={(e) => {
+                  setUnitprice(e.target.value);
+                }}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                value={sellername}
+                class="form-control"
+                id="inputsellername"
+                placeholder="Seller Name"
+                onChange={(e) => {
+                  setSellername(e.target.value);
+                }}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="Number"
+                value={contactno}
+                class="form-control"
+                id="inputcontactnumber"
+                placeholder="Contact Number"
+                onChange={(e) => {
+                  setContactno(e.target.value);
+                }}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                value={description}
+                class="form-control"
+                id="inputdescription"
+                placeholder="Discription"
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+            </div>
+            <div class="custom-file">
+              <input
+                type="file"
+                class="custom-file-input"
+                id="validatedCustomFile"
+                
+              />
+              <p id="pnew">Upload Product Image</p>
+            </div>
+            <div>
+             
+                <button type="submit"  id="btnedit2"> update</button>
+                
+              
+            </div>
+          </form>
      
-      <Link to="/view"><button onClick={selfie} id="btnedit2">Update</button></Link>
+    
 
       
       
-     </form>
+     
  </div>
  
  
